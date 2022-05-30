b28aec16bfa3bb724a57bcbac8857b7b3386e412
binamin-hussein100 <binamin.h.hassan14@gmail.com>
2022-05-30 08:16:10 +0300

    Events complete

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function changeUp(){
+        console.log("Entering password.....")
+    }
+    return(
+        <div>
+            <input onChange={changeUp} type="password"></input>
+        </div>
+    )
+}
+
+export default Keypad
